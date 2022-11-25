7ee02d53ca924e44f6356efe2a379cbe5a70da12
hadilbenyahia <80857931+hadilbenyahia@users.noreply.github.com>
2022-11-25 16:44:19 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bonjour Hadil byahia!");
+        System.out.println("Bonjour Hadil !!!!");
   
     }
 }
